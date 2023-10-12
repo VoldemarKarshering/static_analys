71a01ce25a9c9b65abf126bf5936210806c97032
VoldemarKarshering <kashirin.vova2011@yandex.ru>
2023-10-12 16:15:43 +0300

    task_3

--- task3/task.py
+++ task3/task.py
@@ -1,19 +1,23 @@
 import csv
 from math import log2
 
-def task(file_name):
+def reading(file_name):
     with open(file_name) as file:
         reader = csv.reader(file, delimiter=';')
         matrix = []
         for row in reader:
             matrix.append(row)
-        res = 0.
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if int(matrix[i][j]):
-                    res -= int(matrix[i][j]) / (len(matrix) - 1) *  log2(int(matrix[i][j]) / (len(matrix) - 1))
-        return res
+        return matrix
+
+def task(matrix):
+    res = 0.
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if int(matrix[i][j]):
+                res -= int(matrix[i][j]) / (len(matrix) - 1) *  log2(int(matrix[i][j]) / (len(matrix) - 1))
+    return res
 
 if __name__ == "__main__":
     file_name = input()
-    print(task(file_name))
+    matrix = reading(file_name)
+    print(task(matrix))
