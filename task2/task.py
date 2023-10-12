c001dd569c4b7c7b29209ccfcb48888c16fb53a1
VoldemarKarshering <kashirin.vova2011@yandex.ru>
2023-10-12 16:15:30 +0300

    task_2

--- task2/task.py
+++ task2/task.py
@@ -10,38 +10,42 @@ def writing(file_name):
         writer.writerow(['0', '0', '-1', '0', '0', '0'])
         writer.writerow(['0', '0', '-1', '0', '0', '0'])
 
-def task(file_name):
+def reading(file_name):
     with open(file_name) as file:
         reader = csv.reader(file, delimiter=';')
         matrix = []
         for row in reader:
             matrix.append(row)
-        res = []
-        for i in range(len(matrix)):
-            res.append([0, 0, 0, 0, 0])
-            for j in range(len(matrix[i])):
-                if matrix[i][j] == '1':
-                    res[i][0] += 1
-                    for k in matrix[j]:
-                        if k == '1':
-                            res[i][2] += 1
-                if matrix[i][j] == '-1':
-                    res[i][1] += 1
-                    for k in matrix[j]:
-                        if k == '-1':
-                            res[i][3] += 1
-                        if k == '1':
-                            res[i][4] += 1
+        return matrix
+
+def task(matrix):
+    res = []
+    for i in range(len(matrix)):
+        res.append([0, 0, 0, 0, 0])
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == '1':
+                res[i][0] += 1
+                for k in matrix[j]:
+                    if k == '1':
+                        res[i][2] += 1
+            if matrix[i][j] == '-1':
+                res[i][1] += 1
+                for k in matrix[j]:
+                    if k == '-1':
+                        res[i][3] += 1
+                    if k == '1':
+                        res[i][4] += 1
+    for st in res:
+        if st[4]:
+            st[4] -= 1
+    with open('task2.csv', "w", newline='') as file:
+        writer = csv.writer(file, delimiter=';')
         for st in res:
-            if st[4]:
-                st[4] -= 1
-        with open('task2.csv', "w", newline='') as file:
-            writer = csv.writer(file, delimiter=';')
-            for st in res:
-                writer.writerow(st)
-        return res
+            writer.writerow(st)
+    return res
 
 if __name__ == "__main__":
     file_name = input()
     writing(file_name)
-    print(*task(file_name), sep='\n')
+    matrix = reading(file_name)
+    print(*task(matrix), sep='\n')
